4c0b60154e81de6d9f82458294daad6d769a8603
Mohammed Omar <mohaom@microsoft.com>
2021-05-22 19:19:39 +0300

    Initial

---
+++ src/ConfigMapUtility.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonToConfigMap
+{
+    public static class ConfigMapUtility
+    {
+        public static void ConvertToCM(string JsonFile, string YamlFile, string CMName, bool Tokenize)
+        {
+
+            fillCM(sb, CMName);
+            using (StreamReader sr = new StreamReader(JsonFile))
+            {
+                string json = sr.ReadToEnd();
+                Json4(json, 0,Tokenize);
+                using (StreamWriter sw = new StreamWriter(YamlFile))
+                {
+                    sw.WriteLine(sb.ToString());
+                }
+            }
+        }
+
+        private static void fillCM(StringBuilder sb, string cmName)
+        {
+            sb.AppendLine("apiVersion: v1");
+            sb.AppendLine("kind: ConfigMap");
+            sb.AppendLine("metadata:");
+            sb.AppendLine($"  name: {cmName}");
+            sb.AppendLine("  labels:");
+            sb.AppendLine($"    name: {cmName}");
+            sb.AppendLine("data:");
+        }
+
+        static StringBuilder sb = new StringBuilder();
+        static List<string> rootlevels = new List<string>();
+        static bool Json4(string json, int level, bool tokenize)
+        {
+            if (json == "") return false;
+            Dictionary<string, object> jsonDoc;
+            try
+            {
+                jsonDoc = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            }
+            catch (Exception)
+            {
+                if (tokenize)
+                {
+                    sb.AppendLine($"   \"{rootlevels[level - 1]}\": \"$${rootlevels[level - 1]}$$\"");
+                }
+                else
+                {
+                    sb.AppendLine($"   \"{rootlevels[level - 1]}\": \"{json}\"");
+                }
+
+                rootlevels.RemoveAt(level - 1);
+                return false;
+            }
+
+
+            foreach (string strKey in jsonDoc.Keys)
+            {
+                if (level == 0)
+                {
+                    rootlevels.Clear();
+                    rootlevels.Add(strKey);
+                }
+                else
+                {
+                    rootlevels.Add(rootlevels[level - 1] + "__" + strKey);
+                }
+                Json4(jsonDoc[strKey].ToString(), level + 1,tokenize);
+            }
+            return true;
+        }
+    }
+}
---
+++ src/VSPackage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+using Task = System.Threading.Tasks.Task;
+
+namespace JsonToConfigMap
+{
+    [Guid("2e927fa3-8684-47fc-9674-0046499860d3")] // Must match the GUID in the .vsct file
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("Json To ConfigMap", "", "1.0")]       
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    //[ProvideCodeGenerator(typeof(MinifyCodeGenerator), MinifyCodeGenerator.Name, MinifyCodeGenerator.Description, true)]
+    [ProvideUIContextRule("69760bd3-80f0-4901-818d-c4656aaa08e9", // Must match the GUID in the .vsct file
+        name: "UI Context",
+        expression: "json", // This will make the button only show on .js, .css and .htm(l) files
+        termNames: new[] { "json" },
+        termValues: new[] { "HierSingleSelectionName:.json$" })]
+
+    public sealed class VSPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ApplyCustomTool.InitializeAsync(this);
+
+        }
+    }}
